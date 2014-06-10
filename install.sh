4b1dd9fa03122295b9adf7ccf7d810fe0817c413
Maya Saietz <mayasaietz@gmail.com>
2014-06-10 16:55:01 +0200

    Added installation script (which just makes symlinks)

---
+++ install.sh
@@ -0,0 +1,7 @@
+DIR=`pwd`
+
+ln -sf $DIR/i3status.conf $HOME/.i3status.conf
+ln -sf $DIR/xmonad        $HOME/.xmonad
+ln -sf $DIR/bashrc        $HOME/.bashrc
+ln -sf $DIR/bash_aliases  $HOME/.bash_aliases
+ln -sf $DIR/vimrc         $HOME/.vimrc
